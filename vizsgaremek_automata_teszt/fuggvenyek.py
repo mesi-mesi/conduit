1085e7528097d662e33e8ef088c673727ad71a39
Molnar Emese <varadimesi@gmail.com>
2023-04-06 16:03:21 +0200

    accept_cookie, registration

---
+++ vizsgaremek_automata_teszt/fuggvenyek.py
@@ -0,0 +1,53 @@
+import time
+import csv
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from datetime import datetime
+
+class Fuggvenyek:
+
+    def accept_cookie(self):
+        accept = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located(
+            (By.XPATH, '//button [@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')))
+
+        assert accept.text == 'I accept!'
+
+        accept.click()
+
+    def registration(self):
+        sign_up = self.browser.find_elements(By.XPATH, '//li/a [@class="nav-link"]')[1]
+        sign_up.click()
+        assert self.browser.current_url == 'http://localhost:1667/#/register'
+
+        username = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//input [@placeholder="Username"]')))
+        username.send_keys('user_emese')
+
+        email = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//input [@placeholder="Email"]')))
+        email.send_keys('varadimesi@gmail.com')
+
+        password = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//input [@placeholder="Password"]')))
+        password.send_keys('TesztElek01')
+
+        sign_up_account = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//button [@class="btn btn-lg btn-primary pull-xs-right"]')))
+        sign_up_account.click()
+
+        ok_button = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//button [@class="swal-button swal-button--confirm"]')))
+        ok_button.click()
+
+        user_name = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//a [@href="#/@user_emese/" and @class="nav-link"]')))
+
+        assert user_name.text == 'user_emese'
+        assert self.browser.current_url == 'http://localhost:1667/#/'
---
+++ vizsgaremek_automata_teszt/test_Conduit.py
@@ -0,0 +1,29 @@
+from fuggvenyek import Fuggvenyek
+
+import time
+import csv
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+class TestConduit(Fuggvenyek):
+
+    def setup_method(self):
+        s = Service(executable_path=ChromeDriverManager().install())
+        o = Options()
+        o.add_experimental_option('detach', True)
+        self.browser = webdriver.Chrome(service=s, options=o)
+        url = "http://localhost:1667/#/"
+        self.browser.get(url)
+        self.browser.fullscreen_window()
+
+    def teardown_method(self):
+        self.browser.quit()
+
+    def test_accept_cookie(self):
+        self.accept_cookie()
